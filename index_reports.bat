4f8ab4232cad6d7c912464f9d6f676d4faa55e68
unknown <mseyer@JOHNNY-B.g-cpartners.local>
2015-10-16 15:55:39 -0500

    indexing batch file

---
+++ index_reports.bat
@@ -0,0 +1,6 @@
+elastichandler.py --host 127.0.0.1 --index sbe.test --config etc\sbe_config.json --report report_examples\sbe.donald.usrclass.dat.tsv
+elastichandler.py --host 127.0.0.1 --index sbe.test --config etc\tz_lp_config.json --report report_examples\tz_lnk.txt
+elastichandler.py --host 127.0.0.1 --index sbe.test --config etc\tz_jmp_config.json --report report_examples\tz_jmp.txt
+elastichandler.py --host 127.0.0.1 --index sbe.test --config etc\tz_usp_config.json --report report_examples\tz_usp.txt
+elastichandler.py --host 127.0.0.1 --index sbe.test --config etc\tz_sbag_usrclass_config.json --report report_examples\tz_sbag_usrclass.txt
+elastichandler.py --host 127.0.0.1 --index sbe.test --config etc\tz_sbag_ntuser_config.json --report report_examples\tz_sbag_ntuser.txt
