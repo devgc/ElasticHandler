8dd9271c45a480725c555c99e9a42b2686606002
unknown <mseyer@JOHNNY-B.g-cpartners.local>
2015-10-27 00:08:09 -0500

    Added GcLinkParser output to indexing examples

--- index_reports.bat
+++ index_reports.bat
@@ -4,3 +4,5 @@ elastichandler.py --host 127.0.0.1 --index case_index --config etc\tz_jmp_config
 elastichandler.py --host 127.0.0.1 --index case_index --config etc\tz_usp_config.json --report report_examples\tz_usp.txt
 elastichandler.py --host 127.0.0.1 --index case_index --config etc\tz_sbag_usrclass_config.json --report report_examples\tz_sbag_usrclass.txt
 elastichandler.py --host 127.0.0.1 --index case_index --config etc\tz_sbag_ntuser_config.json --report report_examples\tz_sbag_ntuser.txt
+elastichandler.py --host 127.0.0.1 --index case_index --config etc\gc_link_config.json --report report_examples\gc_lnk_report.json
+elastichandler.py --host 127.0.0.1 --index case_index --config etc\gc_link_config.json --report report_examples\gc_jmp_report.json
