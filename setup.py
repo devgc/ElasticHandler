628f3d100e7d8245d28af1624702d3ee8f51a55d
Erik Kristensen <erik@erikkristensen.com>
2016-01-01 10:16:01 -0700

    adding setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='ElasticHanlder',
+      version='1.0',
+      description='',
+      author='devgc',
+      url='https://github.com/devgc',
+      scripts=['elastichandler.py'],
+      package_data={'ElasticHandler': ['etc/*']}
+     )
